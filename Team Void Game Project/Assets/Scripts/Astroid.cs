cab44c7183f2e68c55fe307c2b6ef8070a268869
Guillermo Belloso <bellosoguillermo@yahoo.com>
2023-09-29 13:45:31 -0700

    Collisions
    Not done but started working on some scripts that work on Astroid, Bullet, and Player Collisions. But the scripts do need polished.

---
+++ Team Void Game Project/Assets/Scripts/Astroid.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Astroid : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Bullet")
+        {
+            Destroy(gameObject);
+        } 
+    }
+}
---
+++ Team Void Game Project/Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float playerSpeed = 10f;
+    // Start is called before the first frame update
+    void Update()
+    {
+        Vector3 newPosition = transform.position + Vector3.up * playerSpeed * Time.deltaTime;
+
+        //Updating Location
+        transform.position = newPosition;
+
+    }
+
+    // Update is called once per frame
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Astroid")
+        {
+            Destroy(gameObject);
+        }
+        
+    }
+}
