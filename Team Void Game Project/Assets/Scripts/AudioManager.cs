ebc180908b9f3612b87290671438f415512f5402
ekuyas <saulmorfin15@gmail.com>
2023-10-06 17:28:55 -0700

    Implemented Sound Effects into GameObject
    Sound effects have been added to a game object named AudioManager, renamed some of the sound effects and added a script to the AudioManager still working on script code for collisions for sound effects.

---
+++ Team Void Game Project/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField] AudioSource SFXSource;
+
+    public AudioClip death;
+    public AudioClip bulletSpawn;
+    public AudioClip bigAsteroidExplosion;
+    public AudioClip mediumAsteroidExplosion;
+    public AudioClip smallAsteroidExplosion;
+
+
+
+
+
+}
